98c67d89c2b5f7b265a6a4a4830c03026fa19a90
Alexander Brandstedt <alexander@infralium.com>
2018-03-28 15:47:35 +0200

    adding deployment make command and deployment k8s template file

--- gen.sh
+++ gen.sh
@@ -19,6 +19,7 @@ cp -r template $GITHUB_REPO
 
 sed -e "s/{{GITHUB_REPO}}/${GITHUB_REPO}/g; s/{{GITHUB_USERNAME}}/${GITHUB_USERNAME}/g;" template/Makefile > $GITHUB_REPO/Makefile
 sed -e "s/{{GITHUB_REPO}}/${GITHUB_REPO}/g; s/{{GITHUB_USERNAME}}/${GITHUB_USERNAME}/g;" template/Dockerfile > $GITHUB_REPO/Dockerfile
+sed -e "s/{{GITHUB_REPO}}/${GITHUB_REPO}/g;" template/template/deployment.yaml > $GITHUB_REPO/template/deployment.yaml
 sed -e "s/{{GITHUB_REPO}}/${GITHUB_REPO}/g;" template/README.md > $GITHUB_REPO/README.md
 sed -e "s/{{GITHUB_REPO}}/${GITHUB_REPO}/g;" template/.gitignore > $GITHUB_REPO/.gitignore
 sed -e "s/{{GITHUB_REPO}}/${GITHUB_REPO}/g;" template/cmd.go > $GITHUB_REPO/cmd.go
