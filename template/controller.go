663a63294ce50a27d440e2505473221e3c6f1fb5
Alexander Brandstedt <alexander@infralium.com>
2018-03-28 12:14:20 +0200

    adding signal handling to allow connection close on sigterm

--- template/controller.go
+++ template/controller.go
@@ -1,25 +1,38 @@
 package main
 
 import (
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 )
 
 type controller struct {
-	interval         time.Duration
-	kube *Kube
+	interval time.Duration
+	kube     *Kube
 }
 
 func newController(kube *Kube, interval time.Duration) *controller {
 	c := &controller{
-		interval:         interval,
-		kube: kube,
+		interval: interval,
+		kube:     kube,
 	}
 	return c
 }
 
-func (c *controller) Run(stopCh chan struct{}) {
+func (c *controller) Run() {
 	log().Info("Starting controller")
 
-	<-stopCh
+	go c.worker()
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, syscall.SIGINT, syscall.SIGTERM)
+	<-signalChan
+
 	log().Info("Stopping controller")
+}
+
+func (c *controller) worker() {
+	for {
+	}
 }
