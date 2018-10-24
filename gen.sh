a7d39fa15c5ab5d3aa09468f5fe875db12da1f8a
Alexander Brandstedt <abrandstedt@spotify.com>
2018-10-24 16:27:25 +0200

    fix bash scripts format to follow same as shfmt

--- gen.sh
+++ gen.sh
@@ -5,8 +5,8 @@ set -o nounset
 set -o pipefail
 
 if [[ -z "${GOPATH}" ]]; then
-    echo "missing env GOPATH in env"
-    exit 1
+	echo "missing env GOPATH in env"
+	exit 1
 fi
 
 read -p 'github username: ' GITHUB_USERNAME
@@ -15,27 +15,27 @@ read -p 'github repo: ' GITHUB_REPO
 GITHUB_USERNAME_DIR="${GOPATH}/src/github.com/${GITHUB_USERNAME}"
 mkdir -p "${GITHUB_USERNAME_DIR}"
 
-cp -r template $GITHUB_REPO
+cp -r template "${GITHUB_REPO}"
 
 find "${GITHUB_REPO}" -type f -print0 | xargs -0 sed -i -e \
-    "s/{{GITHUB_REPO}}/${GITHUB_REPO}/g; s/{{GITHUB_USERNAME}}/${GITHUB_USERNAME}/g;" 
+	"s/{{GITHUB_REPO}}/${GITHUB_REPO}/g; s/{{GITHUB_USERNAME}}/${GITHUB_USERNAME}/g;"
 
 cp -r "${GITHUB_REPO}" "${GITHUB_USERNAME_DIR}"
-rm -r "${GITHUB_REPO}" 
+rm -r "${GITHUB_REPO}"
 
 function setup-git() {
-    git init 
-    git add .
-    git commit -m 'init'
+	git init
+	git add .
+	git commit -m 'init'
 }
 
 function setup-dep() {
 	go get -u github.com/golang/dep/cmd/dep
 	go get -u golang.org/x/tools/cmd/goimports
-    dep ensure -v
+	dep ensure -v
 }
 
 pushd "${GITHUB_USERNAME_DIR}/${GITHUB_REPO}"
-    setup-git
-    setup-dep
+setup-git
+setup-dep
 popd
