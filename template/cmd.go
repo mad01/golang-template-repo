2489e75b787a4ebbd10033469747e4f10b98e7bc
Alexander Brandstedt <alexander.brandstedt@netent.com>
2019-11-28 16:04:46 +0100

    adding more complete cmd

--- template/cmd.go
+++ template/cmd.go
@@ -18,13 +18,46 @@ func cmdVersion() *cobra.Command {
 	return command
 }
 
-func runCmd() error {
-	var rootCmd = &cobra.Command{Use: "{{GITHUB_REPO}}"}
+func cmdRunController() *cobra.Command {
+	var kubeconfig, namespace string
+	var verbose bool
+	var port int
+	var command = &cobra.Command{
+		Use:   "controller",
+		Short: "start the controller",
+		Long:  "",
+		Run: func(cmd *cobra.Command, args []string) {
+			initLog(verbose)
+
+			kube := newKube(kubeconfig)
+
+			controller := newInformer(kube, port)
+			controller.Run()
+		},
+	}
+	command.Flags().StringVarP(
+		&kubeconfig,
+		"kube.config", "k", "",
+		"only needed with running outside cluster, path to kube config")
+	command.Flags().StringVarP(
+		&namespace,
+		"namespace", "n",
+		"default", "ns where the service accounts and cluster role bindings is created",
+	)
+	command.Flags().IntVarP(&port, "http.port", "p", 8080, "port to expose service on")
+	command.Flags().BoolVarP(&verbose, "verbose", "v", false, "verbose output")
+
+	return command
+}
+
+func runCmd() {
+	var rootCmd = &cobra.Command{Use: "k8s-metadata"}
 	rootCmd.AddCommand(cmdVersion())
+	rootCmd.AddCommand(cmdRunController())
 
 	err := rootCmd.Execute()
 	if err != nil {
-		return fmt.Errorf("%v", err.Error())
+		logger.Error(err.Error())
 	}
-	return nil
+	return
 }
