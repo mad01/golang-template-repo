2556ad6d9465f173dc87cea22e35d3cf1962c4e9
Alexander Brandstedt <alexander.brandstedt@netent.com>
2019-11-28 16:02:01 +0100

    update main

--- template/main.go
+++ template/main.go
@@ -1,11 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"os"
+
+	"github.com/sirupsen/logrus"
+)
+
+var logger = &logrus.Entry{}
+
+func initLog(debug bool) {
+	logger = newLogger(debug)
+}
+
+func newLogger(debug bool) *logrus.Entry {
+	l := logrus.Logger{
+		Out:       os.Stdout,
+		Formatter: new(logrus.TextFormatter),
+		Hooks:     make(logrus.LevelHooks),
+		Level:     logrus.InfoLevel,
+	}
+
+	if debug == true {
+		l.SetLevel(logrus.DebugLevel)
+	}
+
+	entry := logrus.NewEntry(&l)
+	return entry
+}
 
 func main() {
 	initLog(false)
-	err := runCmd()
-	if err != nil {
-		fmt.Println(err.Error())
-	}
+	runCmd()
 }
